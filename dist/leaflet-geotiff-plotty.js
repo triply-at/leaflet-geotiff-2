018d228775e479f7a5337c08a65369bc07e17d52
Dan Wild <danwild@y7mail.com>
2020-09-30 14:55:15 +1000

    bugfix plotty renderer uses true displayMax

--- dist/leaflet-geotiff-plotty.js
+++ dist/leaflet-geotiff-plotty.js
@@ -68,7 +68,7 @@
         data: [0],
         width: 1,
         height: 1,
-        domain: [this.options.displayMin, this.options.displayMax + 1],
+        domain: [this.options.displayMin, this.options.displayMax],
         colorScale: this.options.colorScale,
         clampLow: this.options.clampLow,
         clampHigh: this.options.clampHigh
@@ -82,7 +82,7 @@
         // fix for use with rgb conversion (appending alpha channel)
         width: raster.width,
         height: raster.height,
-        domain: [this.options.displayMin, this.options.displayMax + 1],
+        domain: [this.options.displayMin, this.options.displayMax],
         colorScale: this.options.colorScale,
         clampLow: this.options.clampLow,
         clampHigh: this.options.clampHigh,
